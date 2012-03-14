318a61a26dfe81d8b85be4ad17ffbfc3ea7e1a26
Nate Long <long.nathaniel@gmail.com>
2012-03-14 14:49:42 -0700

    changed the namespace name for no real reason, but I like it better :)

--- www-src/js/main.js
+++ www-src/js/main.js
@@ -1,4 +1,6 @@
-goog.require( 'closureBase.templates' );
+goog.require( 'closureBootstrap.templates' );
+
+goog.provide( 'closureBootstrap.main' );
 
 (function(){
 	document.body.innerHTML += closureBase.templates.test({ text: 'Looks like your scripts are working!' });
