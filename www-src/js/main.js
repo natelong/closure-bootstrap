932d2b6dbdbc56f8db3eed3fafae00dcb6f26054
wbyoko <wbagayoko@gmail.com>
2013-01-19 21:26:14 -0600

    closureBase to closureBootstrap

--- www-src/js/main.js
+++ www-src/js/main.js
@@ -3,5 +3,5 @@ goog.require( 'closureBootstrap.templates' );
 goog.provide( 'closureBootstrap.main' );
 
 (function(){
-	document.body.innerHTML += closureBase.templates.test({ text: 'Looks like your scripts are working!' });
+	document.body.innerHTML += closureBootstrap.templates.test({ text: 'Looks like your scripts are working!' });
 }());
